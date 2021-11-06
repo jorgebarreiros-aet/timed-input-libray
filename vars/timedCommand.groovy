0e1715500289479a5fe86f2354c8cfb1d28276f8
jorgebarreiros-aet <barreiros.jorge@gmail.com>
2021-11-06 01:01:57 +0000

    Create timedCommand.groovy

---
+++ vars/timedCommand.groovy
@@ -0,0 +1,15 @@
+// vars/timedCommand.groovy
+def setCommand(commandToRun) {
+cmd = commandToRun
+}
+def getCommand() {
+cmd
+}
+def runCommand() {
+timestamps {
+cmdOut = sh (script:"${cmd}", returnStdout:true).trim()
+}
+}
+def getOutput() {
+cmdOut
+}
