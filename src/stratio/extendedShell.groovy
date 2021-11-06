c9f1d75f7101ba4db65a907266149741a60e5436
jorgebarreiros-aet <barreiros.jorge@gmail.com>
2021-11-06 00:39:34 +0000

    Update extendedShell.groovy

--- src/stratio/extendedShell.groovy
+++ src/stratio/extendedShell.groovy
@@ -5,12 +5,12 @@ def steps
 
 extendedShell(steps){this.steps=steps}
 
-tssh(command-to-run){
+def tssh(command-to-run){
 	def commandOutput
 	steps.timestamps {
 		commandOutput = steps.sh(script: "${<command-to-run>}",
 		returnStdout: true).trim()
 	}	
 	steps.echo commandOutput
-}
+	}
 }
