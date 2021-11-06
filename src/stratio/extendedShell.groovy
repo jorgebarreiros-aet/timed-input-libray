7a814960f6621cac262d735a4ba89f38831c9e0f
jorgebarreiros-aet <barreiros.jorge@gmail.com>
2021-11-06 01:07:36 +0000

    Update extendedShell.groovy

--- src/stratio/extendedShell.groovy
+++ src/stratio/extendedShell.groovy
@@ -1,8 +1,5 @@
 package stratio
 
-class extendedShell implements Serializable{
-def steps
-extendedShell(steps){this.steps=steps}
 def tssh(command-to-run){
 	def commandOutput
 	steps.timestamps {
